db7f9b123083d8a9fe712c147f0c705f52cc4182
Lins <alina.cheremiskina@gmail.com>
2024-12-19 22:19:17 +0300

    save

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        test();
+    }
+
+    private static void test() {
+        Integer number1 = 10;
+        Integer number2 = 0;
+        int result;
+
+        try {
+            result = number1 / number2;
+        } catch (ArithmeticException | NullPointerException e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        }
+    }
+}
